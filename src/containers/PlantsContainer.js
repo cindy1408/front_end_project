2e9a8b68e8bff1318f3bc65e3a9522749c623cbb
cindy1408 <77013299+cindy1408@users.noreply.github.com>
2021-09-27 14:46:35 +0100

    first commit

---
+++ src/containers/PlantsContainer.js
@@ -0,0 +1,10 @@
+class PlantsContainer extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            loaded: false, 
+            plants: []
+        }
+    }
+}
+
---
+++ src/containers/UsersContrainer.js
@@ -0,0 +1,10 @@
+class UsersContainer extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            loaded: false, 
+            users: []
+        }
+    }
+}
+

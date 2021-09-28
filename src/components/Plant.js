468d582db428e0128ec664925cad54c9f00f259c
cindy1408 <77013299+cindy1408@users.noreply.github.com>
2021-09-28 14:18:22 +0100

    plant images

--- src/components/Plant.js
+++ src/components/Plant.js
@@ -1,7 +1,10 @@
+var path = process.env.PUBLIC_URL;
+
 const Plant = (props) => {
     return (
         <div>
             <p>{props.plant.name}</p>
+            <img src = {path + props.image}></img>    
         </div>
     )
 }
--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,13 +1,24 @@
 import Plant from "./Plant"
 
-const PlantList = (props) => {
-    const plantComponents = props.plants.map(plant => {
-        return <Plant plant={plant} />
-    }) 
+var fiddle = './fiddle.jpg';
+var anthurium ='anthurium.jpg';
+var kentia = 'kentiaPalm';
+var cactus = '.img/Cactus';
+var mahonganyFern = '.img/MahoganyFern';
+var birdsOfParadise = '.img/birdsofparadise';
+var lemonTree = '.img/LemonTree';
 
+let plants = [fiddle, anthurium, kentia, cactus, mahonganyFern, birdsOfParadise, lemonTree]
+
+const PlantList = (props) => {
+    let i = 0
+    const plantComponents = props.plants.map(plant => {
+        return <Plant plant={plant} image={plants[i]} />
+    })    
+    i++;
     return (
         <div className="plant">
-        {plantComponents}
+            {plantComponents}
         </div>
     )
 }

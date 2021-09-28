dfd12b529d563d43b15af95bb1c2d83222973dd2
Your Name <you@example.com>
2021-09-28 18:17:41 +0100

    Co-authored-by: farheen931 <farheen931@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import Navbar from './home/components/nav.js';
-import PlantsContainer from './containers/PlantsContainer'
-
-import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import PlantsContainer from './containers/PlantsContainer';
+import './home/home.css';
 
 function App() {
   return (
     <div className="App">
       <PlantsContainer />
+      <Navbar/>
+      {/* <Div></Div>
+      <Div></Div> */}
     </div>
   );
 }

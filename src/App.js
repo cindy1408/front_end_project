5b0feae5bd262fc5b6a24dda4c3c37c1a0b8ab8f
Your Name <you@example.com>
2021-09-29 15:48:40 +0100

    Co-authored-by: farheen931 <farheen931@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import './App.css';
-import Navbar from './home/components/nav.js';
+import Navbar from './components/nav.js';
 import PlantsContainer from './containers/PlantsContainer';
-import './home/home.css';
 
 function App() {
   return (
     <div className="App">
       <PlantsContainer />
       <Navbar/>
-      {/* <Div></Div>
-      <Div></Div> */}
-    </div>
+      <section>
+      <div className="Background">
+        <div className="left">
+          <h1>BUD.DY</h1>
+          <p>—A short introduction to botony</p>
+          </div>
+        <div className="right">
+          <div className="content">
+            <p>Matching you with a best Bud</p>
+            <a href="./quiz.js" className="btn">Take Quiz!</a>
+          </div>
+        </div>
+      </div>
+      </section>
+      </div>
   );
 }
 

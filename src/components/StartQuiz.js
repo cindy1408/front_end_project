eb2ab67ee3007ce158bb9219dc4b8173b905a705
Yulin Chen <elim2015@live.com>
2021-09-30 14:48:00 +0100

    Merged Bianca and Farheen's homepage with links

--- src/components/StartQuiz.js
+++ src/components/StartQuiz.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function StartQuiz(props) {
     return (
         <div>
-            <Link to='./quiz'><button onClick={() => props.startQuiz}>Start Quiz!</button></Link>
+            <Link to='./quiz'><button onClick={() => props.startQuiz}>Take Quiz!</button></Link>
         </div>
     )
 }
